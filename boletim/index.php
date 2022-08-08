d912bba64f23be70cc5acc792a799d3a67a64cbb
xviigimenez <gimenez@tuta.io>
2022-08-08 09:37:54 -0300

    começada a tarefa do boletim

---
+++ boletim/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Front-end</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1><a href="index.php">Boletim</a></h1>
+        </header>
+        <main>
+            <article>
+                <h1>Este é um artigo.</h1>
+                <img style="width=95%; height: 95%; margin: 5%" src="tux.webp">
+                <p>O usuário de Windows teme o Tux terrorista.</p>
+            </article>
+        </main>
+        <footer>
+            <p>Essa página foi criada por <a href="mailto:gustavo.correa38@etec.sp.gov.br">Gustavo Gimenez Correa</a> e <a href="gabriel.daloco@etec.sp.gov.br">Gabriel Rangel Maciel Daloco</a>.</p>
+        </footer>
+    </body>
+</html>
