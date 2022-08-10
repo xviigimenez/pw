07bc993ec159ab6b2ce5800fa12c76302c426803
xviigimenez <gimenez@tuta.io>
2022-08-10 15:36:10 -0300

    modificada a página index do boletim

--- boletim/index.php
+++ boletim/index.php
@@ -10,11 +10,17 @@
             <h1><a href="index.php">Boletim</a></h1>
         </header>
         <main>
-            <article>
-                <h1>Este é um artigo.</h1>
-                <img style="width=95%; height: 95%; margin: 5%" src="tux.webp">
-                <p>O usuário de Windows teme o Tux terrorista.</p>
-            </article>
+            <label>Matrícula</label>
+            <input type="number" name="matricula"><br>
+            <label>Nome</label>
+                    <input type="text" name="nome"><br>
+            <label>Nota 1</label>
+                    <input type="number" name="nota1"><br>
+            <label>Nota 2</label>
+                    <input type="number" name="nota2"><br>
+            <label>Nota 3</label>
+                    <input type="number" name="nota3"><br>
+            <label>Média:</label>
         </main>
         <footer>
             <p>Essa página foi criada por <a href="mailto:gustavo.correa38@etec.sp.gov.br">Gustavo Gimenez Correa</a> e <a href="gabriel.daloco@etec.sp.gov.br">Gabriel Rangel Maciel Daloco</a>.</p>
