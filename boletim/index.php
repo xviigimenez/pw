f33bf8d5b5bcab63b9685d7271dbe78c295ac12c
xviigimenez <gimenez@tuta.io>
2022-08-12 23:06:10 -0300

    reformulada completamente a página inicial, com CSS atualizado e cautelosamente alinhado; também adicionado um favicon e consertado um erro em um dos links de email

--- boletim/index.php
+++ boletim/index.php
@@ -1,29 +1,33 @@
 <!doctype html>
 <html>
     <head>
-        <title>Front-end</title>
+        <title>Boletim</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
     </head>
     <body>
         <header>
-            <h1><a href="index.php">Boletim</a></h1>
+            <h1>Boletim</h1>
         </header>
         <main>
-            <label>Matrícula</label>
-            <input type="number" name="matricula"><br>
-            <label>Nome</label>
-                    <input type="text" name="nome"><br>
-            <label>Nota 1</label>
-                    <input type="number" name="nota1"><br>
-            <label>Nota 2</label>
-                    <input type="number" name="nota2"><br>
-            <label>Nota 3</label>
-                    <input type="number" name="nota3"><br>
-            <label>Média:</label>
+            <div>
+                <label>Matrícula</label>
+                <input type="number" name="matricula">
+                <label>Nome</label>
+                <input type="text" name="nome">
+                <label>Nota 1</label>
+                <input type="number" name="nota1">
+                <label>Nota 2</label>
+                <input type="number" name="nota2">
+                <label>Nota 3</label>
+                <input type="number" name="nota3">
+            </div>
         </main>
         <footer>
-            <p>Essa página foi criada por <a href="mailto:gustavo.correa38@etec.sp.gov.br">Gustavo Gimenez Correa</a> e <a href="gabriel.daloco@etec.sp.gov.br">Gabriel Rangel Maciel Daloco</a>.</p>
+        <div>
+            <p>Esta página foi criada por <a href="mailto:gustavo.correa38@etec.sp.gov.br">Gustavo Gimenez Correa</a> e <a href="mailto:gabriel.daloco@etec.sp.gov.br">Gabriel Rangel Maciel Daloco</a>.</p>
+        </div>
         </footer>
     </body>
 </html>
