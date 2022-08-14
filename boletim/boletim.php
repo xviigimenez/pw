87abe049b15e2322518612d6855713fa97053ed3
GabrielDaloco <daloco.gabriel@gmail.com>
2022-08-13 22:34:10 -0300

    (boletim) adicionado php.

---
+++ boletim/boletim.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once "clsBoletim.php";
+
+$vlBoletim = new clsBoletim;
+
+$matricula = $_POST['matricula'];
+$aluno = $_POST['nome'];
+
+$vlBoletim->setNota1($_POST['nota1']);
+$vlBoletim->setNota2($_POST['nota2']);
+$vlBoletim->setNota3($_POST['nota3']);
+
+echo "** BOLETIM ** <br>";
+echo "Matricula: " . $matricula . " | ";
+echo "Aluno: " . $aluno . "<br>";
+echo "com notas " . $vlBoletim->getNota1() . ", " . $vlBoletim->getNota2() . ", " . $vlBoletim->getNota3() . ".<br>";
+echo "Ficou com media " . $vlBoletim->gerarBoletim() . "<br>";
+echo "Situação " . $vlBoletim->getSituacao();
+?>
