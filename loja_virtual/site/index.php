4147b2774824879426c817d5fc14864b01f3b526
xviigimenez <gimenez@tuta.io>
2022-09-01 21:01:05 -0300

    (loja_virtual) criada a estrutura de arquivos e o rascunho da página inicial

---
+++ loja_virtual/site/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Loja virtual</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="icon" type="image/x-icon" href="css/favicon.ico">
+    </head>
+    <body>
+        <header>
+            <h1>Loja virtual</h1>
+            <nav>
+                <ul>
+                    <li><a href="cadastro.php">Cadastro</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>
