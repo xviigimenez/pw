89734130a5a01ff5bf08181794b01b2b3879b2c2
xviigimenez <gimenez@tuta.io>
2022-09-05 09:37:40 -0300

    (loja_virtual) começado o design da página inicial

--- loja_virtual/site/index.php
+++ loja_virtual/site/index.php
@@ -1,21 +1,32 @@
 <!doctype html>
 <html>
     <head>
-        <title>Loja virtual</title>
+        <title>J&R - Jimas & Rangel</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="icon" type="image/x-icon" href="css/favicon.ico">
     </head>
     <body>
         <header>
-            <h1>Loja virtual</h1>
-            <nav>
-                <ul>
-                    <li><a href="cadastro.php">Cadastro</a></li>
-                </ul>
-            </nav>
+            <div class="logo">
+                <h1><a href="index.php">J&R</a></h1>
+            </div>
+            <div class="nav">
+                <nav>
+                    <ul>
+                        <li><a href="cadastro.php">Cadastro</a></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
         <main>
+            <div class="carrossel">
+                <img src="img/banner.jpg" alt="J&R">
+            </div>
+            <div class="cards">
+                <div class="card">
+                </div>
+            </div>
         </main>
         <footer>
         </footer>
